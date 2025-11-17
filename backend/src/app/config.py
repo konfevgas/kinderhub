ec2553c6649c7517e041a9f3bf1317d91067421d
Konstantinos Fevgas <kon.fevgas@gmail.com>
2025-11-17 09:11:48 +0200

    Code formatting

--- backend/src/app/config.py
+++ backend/src/app/config.py
@@ -1,13 +1,16 @@
-from pydantic import BaseModel
-from dotenv import load_dotenv
 import os
 
+from dotenv import load_dotenv
+from pydantic import BaseModel
+
 load_dotenv()
+
 
 class Settings(BaseModel):
     database_url: str = os.getenv(
         "DATABASE_URL",
-        "postgresql+psycopg://USER:PASSWORD@localhost:5432/DATABASE_NAME"
+        "postgresql+psycopg://USER:PASSWORD@localhost:5432/DATABASE_NAME",
     )
+
 
 settings = Settings()
